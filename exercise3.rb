2dab5f5564237a1f370b786d41f368b202777eed
chau nguyen <nhbc111@yahoo.com>
2016-05-31 18:12:09 -0400

    added something

--- exercise3.rb
+++ exercise3.rb
@@ -2,6 +2,6 @@ puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
 
-puts "What year you were born?"
-year = gets.chomp
-puts "#{name} is #{2016 - 1984} years old"
+puts "How old are you?"
+age = gets.chomp
+puts "#{name} were born #{2016 - age.to_i}"
