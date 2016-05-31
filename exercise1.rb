91af7f7374117ca4b687fa9f64391a4d3e1762f1
chau nguyen <nhbc111@yahoo.com>
2016-05-31 16:06:59 -0400

    added somthings

--- exercise1.rb
+++ exercise1.rb
@@ -13,3 +13,8 @@ puts 'Betty\'s pie shop'
 
 puts "Ada Lovelace lived for #{1852 - 1815} years"
 #Ada Lovelace lived for 37 years.
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bgoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
