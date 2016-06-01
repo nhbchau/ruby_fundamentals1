5ffd9510ded4a72ae9a86a63c5e2ae85fc8a9205
chau nguyen <nhbc111@yahoo.com>
2016-06-01 10:03:23 -0400

    added somthing

--- exercise4.rb
+++ exercise4.rb
@@ -1,28 +1,25 @@
 #counter = 1
-#x = 3
-#y = 5
 #while counter < 101
-  #puts "#{counter}"
-  #if (counter % x) == 0
+  #  puts "BitMaker"
+  #elsif (counter % 3) == 0
   #  puts "Bit"
-  #elsif (counter % y) == 0
-  #  puts "Maker"
-  #else
-  #  puts "#{counter}"
+  #elsif (counter % 5) == 0
+    #puts "Maker"
+    #else
+   #puts counter
   #end
  #counter += 1
 #end
 
 one_to_hundred = (1..100)
-one_to_hundred.each do |x|
-  if (x % 3) + (x % 5 )== 0
+one_to_hundred.each do |num|
+  if (num % 3) && (num % 5 )== 0
     puts "BitMaker"
-  elsif (x % 3) == 0
+  elsif (num % 3) == 0
     puts "Bit"
-  else (x % 5) == 0
+  elsif (num % 5) == 0
     puts "Maker"
-  end
-  unless x % 3 == 0 || x % 5 == 0
-    puts "#{x}"
+  else
+    puts num
   end
 end
